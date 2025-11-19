7dc019fb3b062dd8c7832c6e51d6e8f5bc515215
houxq8888 <houxq8888@126.com>
2025-11-19 09:24:54 +0800

    可编译成功，运行

---
+++ build/CMakeFiles/software-editor.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/software-editor.dir/main.cpp.obj"
+  "CMakeFiles/software-editor.dir/main.cpp.obj.d"
+  "CMakeFiles/software-editor.dir/mainwindow.cpp.obj"
+  "CMakeFiles/software-editor.dir/mainwindow.cpp.obj.d"
+  "CMakeFiles/software-editor.dir/previewwindow.cpp.obj"
+  "CMakeFiles/software-editor.dir/previewwindow.cpp.obj.d"
+  "CMakeFiles/software-editor.dir/product.cpp.obj"
+  "CMakeFiles/software-editor.dir/product.cpp.obj.d"
+  "CMakeFiles/software-editor.dir/software-editor_autogen/mocs_compilation.cpp.obj"
+  "CMakeFiles/software-editor.dir/software-editor_autogen/mocs_compilation.cpp.obj.d"
+  "CMakeFiles/software-editor.dir/uilayoutwindow.cpp.obj"
+  "CMakeFiles/software-editor.dir/uilayoutwindow.cpp.obj.d"
+  "CMakeFiles/software-editor_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/software-editor_autogen.dir/ParseCache.txt"
+  "software-editor_autogen"
+  "libsoftware-editor.dll.a"
+  "software-editor.exe"
+  "software-editor.exe.manifest"
+  "software-editor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/software-editor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/software-editor_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/software-editor_autogen"
+  "software-editor_autogen/include/ui_mainwindow.h"
+  "software-editor_autogen/include/ui_previewwindow.h"
+  "software-editor_autogen/include/ui_uilayoutwindow.h"
+  "software-editor_autogen/mocs_compilation.cpp"
+  "software-editor_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/software-editor_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
